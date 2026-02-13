33cae792001e1ec59cced9d17ddc5c1e65c68e75
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2026-02-13 16:54:04 +0100

    fix mergetool

--- htdocs/admin/tools/ui/class/documentation.class.php
+++ htdocs/admin/tools/ui/class/documentation.class.php
@@ -280,40 +280,6 @@ class Documentation
 						'ExperimentalUxContributionTitle' => '#experimental-ux-contribution',
 					),
 				),
-				'ExperimentalUxInputAjaxFeedback' => array(
-					'url' => dol_buildpath($this->baseUrl.'/experimental/experiments/input-feedback/index.php', 1),
-					'icon' => 'fas fa-flask',
-					'submenu' => array(),
-					'summary' => array(),
-				),
-				'UxDolibarrContext' => array(
-					'url' => dol_buildpath($this->baseUrl.'/experimental/experiments/dolibarr-context/index.php', 1),
-					'icon' => 'fas fa-flask',
-					'submenu' => array(
-						'UxDolibarrContextHowItWork' => array(
-							'url' => dol_buildpath($this->baseUrl.'/experimental/experiments/dolibarr-context/index.php', 1),
-							'icon' => 'fas fa-flask',
-							'submenu' => array(),
-							'summary' => array(
-								'Introduction' => '#titlesection-basicusage',
-								'ConsoleHelp' => '#titlesection-console-help',
-								'JSDolibarrhooks' => '#titlesection-hooks',
-								'JSDolibarrhooksReadyVsInit' => '#titlesection-event-init-vs-ready',
-								'JSDolibarrAwaitHooks' => '#titlesection-await-hooks',
-								'ExampleOfCreatingNewContextTool' => '#titlesection-create-tool-example',
-								'SetEventMessageTool' => '#titlesection-tool-seteventmessage',
-								'SetAndUseContextVars' => '#titlesection-contextvars',
-							),
-						),
-						'UxDolibarrContextLangsTool' => array(
-							'url' => dol_buildpath($this->baseUrl.'/experimental/experiments/dolibarr-context/langs-tool.php', 1),
-							'icon' => 'fas fa-flask',
-							'submenu' => array(),
-							'summary' => array(),
-						),
-					),
-					'summary' => array(),
-				),
 				'ExperimentalUxInputFileDropZone' => array(
 					'url' => dol_buildpath($this->baseUrl.'/experimental/experiments/inputfile-dropzone/index.php', 1),
 					'icon' => 'fas fa-flask',
